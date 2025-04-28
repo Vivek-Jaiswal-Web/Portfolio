38bdc7fd817dad607a32d3d47238f5b35f9b87a1
Vivek Jaiswal <vivekrkjaiswal@gmail.com>
2025-04-28 20:31:19 +0530

    changes

--- src/components/About.js
+++ src/components/About.js
@@ -32,7 +32,7 @@ const About = () => {
             <div className="section__pic-container animate">
               <img
                 src="./assets/person stand.png"
-                alt="A person standing illustration for About Me section"
+                alt=""
                 className="about-pic"
               />
             </div>
